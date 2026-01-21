36876e2d4106eb033f0a920b4ef7cb64e9ae1301
jane872 <8729468891jane@gmail.com>
2026-01-20 20:05:10 -0600

    Update test_imports.py

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -3,14 +3,14 @@
 def test_numpy_import():
  
     import numpy
-    assert numpy.__version__ == "1.24.3", f"expect numpy 1.24.3，actual{numpy.__version__}"
+    assert numpy.__version__ == "1.26.3", f"expect numpy 1.26.3，actual{numpy.__version__}"
 
 def test_pandas_import():
     
     import pandas
-    assert pandas.__version__ == "2.0.3", f"expect pandas 2.0.3，actual{pandas.__version__}"
+    assert pandas.__version__ == "2.1.4", f"expect pandas 2.1.4，actual{pandas.__version__}"
 
 def test_pytest_import():
    
     import pytest
-    assert pytest.__version__ == "7.4.0", f"expect pytest 7.4.0，actual{pytest.__version__}"
+    assert pytest.__version__ == "9.0.2", f"expect pytest 9.0.2，actual{pytest.__version__}"
